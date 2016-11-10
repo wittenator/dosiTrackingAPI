654ba755d965ec880d0ced070c234712a62044ce
wittenator <wittenator@yahoo.de>
2016-11-10 12:11:17 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+
+});
+
+// CONTACTS API ROUTES BELOW
