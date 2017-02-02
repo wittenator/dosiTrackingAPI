768f82b2ee265fbc7613eb21be946413a04c2230
wittenator <wittenator@yahoo.de>
2017-02-02 14:42:26 +0100

    new try

--- server/sql_queries/database_struct.sql
+++ server/sql_queries/database_struct.sql
@@ -23,13 +23,28 @@ VALUES
 /* inserts test values*/
 INSERT INTO people (rfid, name, lastname, ressort)
 VALUES 
-  (1, 'Tim', 'Korjakow', 'Juniter'),
-  (2, 'Axel', 'Bernhard', 'Juniter'),
-  (3, 'Manuel', 'Schültke', 'Human Ressources');
+  (3966584180, 'Caterina', 'Betz', 'HR'),
+  (2365005684, 'Hans', 'Hosenfeld', 'IT'),
+  (3960083572, 'Irma', 'Klünker', 'QM'),
+  (208489076, 'Jonas', 'Zell', 'QM'),
+  (2387422811, 'Lennart', 'Berger', 'RM'),
+  (3155969396, 'Lukas', 'Hoffmann', 'IT'),
+  (3960571764, 'Matthias', 'Göttl', 'Vorstand'),
+  (2091137140, 'Maximilian', 'Eißler', 'Vorstand'),
+  (1022571892, 'Maximilian', 'Gracz', 'QM'),
+  (1556629876, 'Nils', 'Kassebohm', 'RM'),
+  (3702414964, 'Philip', 'Stange', 'QM'),
+  (1011844468, 'Philip', 'Hasenklever', 'QM'),
+  (472933748, 'Shakhlo', 'Abduganieva', 'HR'),
+  (2889517940, 'Shalin Sara', 'Baier', 'QM'),
+  (1556235124, 'Timur', 'Basilik', 'HR'),
+  (1009406836, 'Yannik', 'Schmid', 'IN'),
+  (2084317556, 'Zebo', 'Abduganieva', 'Vorstand'),
+  (1818972020, 'Patrick', 'Armanious', 'QM');
   
 INSERT INTO sessions (name, type, starttime, endtime)
 VALUES 
-  ('Dosi am 12.12.2016', 'DoSi', '2016-12-12 18:30:00', '2016-12-12 20:10:00'),
+  ('Testevent', 'DoSi', '2017-02-02 11:00:00', '2017-02-02 15:00:00'),
   ('PR Schulung', 'Schulung', '2016-12-15 10:40:00', '2016-12-15 14:50:00'),
   ('DoSi am 19.12', 'DoSi', '2016-12-19 18:30:00', '2016-12-19 22:30:00');
   
